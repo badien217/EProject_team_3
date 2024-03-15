af108150da01a4c6e1c930a50e0cf8f6a2461772
thao <nguyenthao21304@gmail.com>
2024-03-15 18:42:28 +0700

    run

--- FrontEnd/angular/src/app/interfaces/feedback.ts
+++
@@ -1,8 +0,0 @@
-export interface Feedback {
-    id?: number;
-    name?: string;
-    email?: string;
-    phone?: string;
-    feedbackContent?: string;
-    feedbackDate?: Date;
-}
--- FrontEnd/angular/src/app/interfaces/product.ts
+++
@@ -1,7 +0,0 @@
-export interface Product {
-    id?: number;
-    name?: string;
-    flavorId?: number;
-    flavorName?: string;
-    imageUrl?: string;
-}
